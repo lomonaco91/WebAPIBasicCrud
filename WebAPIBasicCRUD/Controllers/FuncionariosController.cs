3e32e3b98459555893fd3c3ef76ee46544cd62f8
Fabiano Júnior <fabiano.lomonaco.junior@gmail.com>
2018-04-06 19:16:37 -0300

    Commiting test files

---
+++ WebAPIBasicCRUD/Controllers/FuncionariosController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebAPIBasicCRUD.Models;
+
+namespace WebAPIBasicCRUD.Controllers
+{
+    public class FuncionariosController : ApiController
+    {
+        //static list
+        private static List<Funcionario> fun = new List<Funcionario>();
+
+        //get all
+        public List<Funcionario> Get()
+        {
+            return fun;
+        }
+
+        //create
+        public HttpResponseMessage Post ( string name, string email, string department)
+        {
+
+            //Any blank field = err
+            if ((string.IsNullOrEmpty(name)) || (string.IsNullOrEmpty(email)) || (string.IsNullOrEmpty(department)))
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest, "Check empty fields!");
+            }
+            else
+            {
+                fun.Add(new Funcionario( name, email, department));
+                return Request.CreateResponse(HttpStatusCode.Created, "Employee Created.");
+            }
+        }
+
+        //delete by name
+        public HttpResponseMessage Delete(string name)
+        {
+
+            //blank field = err
+            if (string.IsNullOrEmpty(name))
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest, "Could not find a employee to delete");
+            }
+            else
+            {
+                fun.RemoveAt(fun.IndexOf(fun.First(x => x.Name.Equals(name))));
+                return Request.CreateResponse(HttpStatusCode.OK, "Employee Deleted");
+            }
+
+        }
+    }
+}
+
---
+++ WebAPIBasicCRUD/Models/Funcionario.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAPIBasicCRUD.Models
+{
+    public class Funcionario
+    {
+        public string Name { get; set; }
+        public string Mail { get; set; }
+        public string Dep { get; set; }
+
+        public Funcionario ( string name, string mail, string dep)
+        {
+            this.Name = name;
+            this.Mail = mail;
+            this.Dep = dep;
+
+        }
+    }
+}
+
